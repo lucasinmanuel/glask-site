e78c5d15c071bb69e967b7a2ad74ae37d5b31272
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-04-09 14:55:08 -0300

    fixed  imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 import Home from './pages/Home/Home';
 import Allocation from './pages/Allocation/Allocation';
 
