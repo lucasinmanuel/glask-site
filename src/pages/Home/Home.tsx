48ddf5560bead5bca1c2973a2fac2ab5b202c18c
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-04-14 22:49:15 -0300

    adding transaction status

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,10 +19,10 @@ import {
   Transaction,
   SystemProgram,
   LAMPORTS_PER_SOL,
+  Keypair,
 } from "@solana/web3.js";
 import {
   LedgerWalletAdapter,
-  PhantomWalletAdapter,
   SolflareWalletAdapter,
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
@@ -30,22 +30,54 @@ import {
 require("@solana/wallet-adapter-react-ui/styles.css");
 require("./home.css");
 
+const TARGET_BLOCK_TIME = 5000; // Tempo alvo de um bloco em milissegundos
+
 const MyComponent = () => {
+
+  
   const { publicKey, sendTransaction } = useWallet(); // Obtém a carteira da pessoa conectada
   const { connection } = useConnection();
 
   const [sol, setSol] = useState<number>(0);
   const [bal, setBal] = useState<number>(0);
+  const [status, setStatus] = useState<"Transação enviada"|"Transação pendente"|"Transação confirmada"|"Transação falhada"|"">("");
+  const [networkActivity, setNetworkActivity] = useState<string>("Normal"); // Inicializa a atividade da rede como "Normal"
+
+
+  const fetchBalance = async () => {
+    if (publicKey && connection) {
+      const balance = await connection.getBalance(publicKey);
+      setBal(balance / LAMPORTS_PER_SOL);
+    }
+  };
 
   useEffect(() => {
-    const fetchBalance = async () => {
-      if (publicKey && connection) {
-        const balance = await connection.getBalance(publicKey);
-        setBal(balance / LAMPORTS_PER_SOL);
-      }
-    };
     fetchBalance();
   }, [publicKey, connection]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      monitorNetworkActivity(); // Chama a função monitorNetworkActivity a cada intervalo
+    }, 60000); // Atualiza a cada 1 minuto
+    return () => clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
+  }, []);
+
+  const monitorNetworkActivity = async () => {
+    try {
+      const networkInfo = await connection.getEpochInfo(); // Obtem informações sobre a época atual da rede Solana
+      const blockTime = await connection.getBlockTime(networkInfo.absoluteSlot); // Calcula o tempo de um bloco
+
+      if(blockTime){
+        if (blockTime < TARGET_BLOCK_TIME) {
+          setNetworkActivity("High"); // Define a atividade da rede como alta se o tempo médio do bloco for menor que o alvo
+        } else {
+          setNetworkActivity("Normal"); // Define a atividade da rede como normal caso contrário
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao obter informações de rede:", error);
+    }
+  };
 
   const transferSol = async () => {
     if (!publicKey) throw new WalletNotConnectedError();
@@ -65,7 +97,16 @@ const MyComponent = () => {
         })
       );
 
+      // Defina o `feePayer` para uma carteira que tenha saldo suficiente para cobrir uma taxa mais alta
+      transaction.feePayer = publicKey;
+
+      transaction.recentBlockhash = latestBlockHash.blockhash;
+
+      setStatus("Transação pendente");
+
       const signature = await sendTransaction(transaction, connection);
+
+      setStatus("Transação enviada");
 
       console.log("Transferência enviada:", signature);
 
@@ -79,7 +120,15 @@ const MyComponent = () => {
       );
 
       console.log("Status da transação:", confirmation);
+
+      if(confirmation.value.err == null){
+        setStatus("Transação confirmada");
+        fetchBalance();
+      }else{
+        setStatus("Transação falhada");
+      }
     } catch (error) {
+      setStatus("Transação falhada");
       console.error("Falha ao enviar transferência:", error);
     }
   };
@@ -94,13 +143,17 @@ const MyComponent = () => {
         <WalletMultiButton />
         {publicKey ? (
           <div>
-            <p>my balance: {bal}</p>
+            <p>My Balance: {bal}</p>
+            <p>Network Activity: {networkActivity}</p> {/* Exibe a atividade da rede */}
             <input
               value={sol}
               type="number"
               onChange={(e: any) => setSol(e.target?.value)}
             ></input>
             <button onClick={transferSol}>Transferir SOL</button>
+            {
+              status != "" && <p>{status}</p>
+            }
           </div>
         ) : (
           <p>Nenhuma carteira conectada</p>
@@ -111,19 +164,18 @@ const MyComponent = () => {
 };
 
 function Home() {
-  const network = WalletAdapterNetwork.Mainnet; // Devnet é teste. Você pode alterar para 'MainnetBeta' para a rede principal
+  const network = WalletAdapterNetwork.Devnet; // Devnet é teste. Você pode alterar para 'MainnetBeta' para a rede principal
   const endpoint = clusterApiUrl(network, true);
 
   const wallets = useMemo(
     () => [
       new LedgerWalletAdapter(),
-      new PhantomWalletAdapter({ network }),
       new SolflareWalletAdapter({ network }),
       new TorusWalletAdapter({ params: { network } }),
     ],
     [network]
   );
-
+  // https://rough-proportionate-mountain.solana-mainnet.quiknode.pro/f91fe767abcd15798f217e45883093b94d5ac81d/
   return (
     <main>
       <ConnectionProvider endpoint={endpoint}>
