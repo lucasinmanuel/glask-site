ca510b4942992c934b97f39372fc05436d809f21
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-04-27 02:41:23 -0300

    conecting API

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -30,7 +30,6 @@ const Home = () => {
   const [sol, setSol] = useState<number>(0);
   const [bal, setBal] = useState<number>(0);
   const [status, setStatus] = useState<"Transação enviada"|"Transação pendente"|"Transação confirmada"|"Transação falhada"|"">("");
-  const [networkActivity, setNetworkActivity] = useState<string>("Normal"); // Inicializa a atividade da rede como "Normal"
 
   const fetchBalance = async () => {
     if (publicKey && connection) {
@@ -42,30 +41,6 @@ const Home = () => {
   useEffect(() => {
     fetchBalance();
   }, [publicKey, connection]);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      monitorNetworkActivity(); // Chama a função monitorNetworkActivity a cada intervalo
-    }, 60000); // Atualiza a cada 1 minuto
-    return () => clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
-  }, []);
-
-  const monitorNetworkActivity = async () => {
-    try {
-      const networkInfo = await connection.getEpochInfo(); // Obtem informações sobre a época atual da rede Solana
-      const blockTime = await connection.getBlockTime(networkInfo.absoluteSlot); // Calcula o tempo de um bloco
-
-      if(blockTime){
-        if (blockTime < TARGET_BLOCK_TIME) {
-          setNetworkActivity("High"); // Define a atividade da rede como alta se o tempo médio do bloco for menor que o alvo
-        } else {
-          setNetworkActivity("Normal"); // Define a atividade da rede como normal caso contrário
-        }
-      }
-    } catch (error) {
-      console.error("Erro ao obter informações de rede:", error);
-    }
-  };
 
   const transferSol = async () => {
     if ( !publicKey ) throw new WalletNotConnectedError();
@@ -131,7 +106,6 @@ const Home = () => {
         {publicKey ? (
           <div>
             <p>My Balance: {bal}</p>
-            <p>Network Activity: {networkActivity}</p> {/* Exibe a atividade da rede */}
             <input
               value={sol}
               type="number"
