a1f03f2a7b0a12c5723ab84c48606e6019c8322a
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-04-28 15:21:05 -0300

    adicioando alternador de RPCS

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Home from './pages/Home/Home';
 import Allocation from './pages/Allocation/Allocation';
 import { WalletContextProvider } from './contexts/WalletContext';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { clusterApiUrl } from '@solana/web3.js';
-import { useMemo } from 'react';
+import { Connection, clusterApiUrl } from '@solana/web3.js';
+import { useEffect, useMemo, useState } from 'react';
 import { LedgerWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
@@ -15,9 +15,50 @@ function App() {
   if (typeof window !== 'undefined' && !window.Buffer) {
     window.Buffer = require('buffer').Buffer;
   }
-  const network = WalletAdapterNetwork.Devnet; // Devnet é teste. Você pode alterar para 'MainnetBeta' para a rede principal
-  const endpoint = clusterApiUrl(network, true);
+  const [endpoint,setEndpoint] = useState("https://api.devnet.solana.com");
+  // const [rpcListIndex,setRpcListIndex] = useState(0);
 
+  // // Lista de RPCs Solana
+  // const rpcList = [
+  //   'https://api.mainnet-beta.solana.com', // Exemplo de RPC Solana principal
+  //   'https://api.devnet.solana.com', // Exemplo de RPC Solana de desenvolvimento
+  //   // Adicione outros RPCs Solana que deseja usar como alternativas
+  // ];
+
+  // // Função para verificar a disponibilidade do RPC
+  // async function checkRPCAvailability(rpcUrl:string) {
+  //   const web3 = new Connection(rpcUrl);
+  //   try {
+  //       // Realize uma chamada simples para verificar a disponibilidade
+  //       const blockCount = await web3.getLatestBlockhash();
+  //       console.log(`RPC ${rpcUrl} está online. Último bloco: ${blockCount}`);
+  //       return true;
+  //   } catch (error:any) {
+  //       console.log(`Erro ao se conectar ao RPC ${rpcUrl}: ${error.message}`);
+  //       return false;
+  //   }
+  // }
+
+  // async function verify() {
+  //   const isAvailable = await checkRPCAvailability(rpcList[rpcListIndex]);
+  //   if (!isAvailable) {
+  //     const nextIndex = rpcListIndex + 1;
+  //     if (nextIndex < rpcList.length && await checkRPCAvailability(rpcList[nextIndex])) {
+  //       console.log(`Alternando para ${rpcList[nextIndex]}`);
+  //       setRpcListIndex(nextIndex);
+  //       setEndpoint(rpcList[nextIndex]);
+  //     }
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     verify();
+  //   }, 10000);
+  //   return () => clearInterval(intervalId);
+  // }, []);
+  
+  const network = WalletAdapterNetwork.Devnet; // Devnet é teste. Você pode alterar para 'MainnetBeta' para a rede principal
   const wallets = useMemo(
     () => [
       new LedgerWalletAdapter(),
@@ -26,6 +67,7 @@ function App() {
     ],
     [network]
   );
+
   return (
     <WalletContextProvider>
       <BrowserRouter>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,36 +14,36 @@ function Header() {
             setRanking(data)
         })
     },[])
-  return (
-    <header>
-        <div className="container">
-            <img src={logo} alt="logo" className="logo" />
-            <nav>
-                <ul>
-                    <li>
-                        <Link target="_blank" to="/">$glask</Link>
-                    </li>
-                    <li>
-                        <Link to="/">home</Link>
-                    </li>
-                    <li>
-                        <Link to="/allocation">allocation</Link>
-                    </li>
-                    <li>
-                        <Link target="_blank" to="/">whitepaper</Link>
-                    </li>
-                    <li>
-                        <Link target="_blank" to="/">telegram</Link>
-                    </li>
-                    <li>
-                        <Link target="_blank" to="/">twitter</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <Outlet />
-    </header>
-  );
+    return (
+        <header>
+            <div className="container">
+                <img src={logo} alt="logo" className="logo" />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link target="_blank" to="/">$glask</Link>
+                        </li>
+                        <li>
+                            <Link to="/">home</Link>
+                        </li>
+                        <li>
+                            <Link to="/allocation">allocation</Link>
+                        </li>
+                        <li>
+                            <Link target="_blank" to="/">whitepaper</Link>
+                        </li>
+                        <li>
+                            <Link target="_blank" to="/">telegram</Link>
+                        </li>
+                        <li>
+                            <Link target="_blank" to="/">twitter</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Outlet />
+        </header>
+    );
 }
 
 export default Header;
